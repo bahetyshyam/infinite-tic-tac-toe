a6fb5711ce744229ea31f3129fe355a0ea19e527
bahetyshyam <bahetyshyam@gmail.com>
2024-07-20 03:06:33 +0530

    Added google analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,10 @@
 import "./App.css";
 import { TicTacToe } from "./components/TicTacToe";
+import ReactGA from "react-ga4";
+
+if (import.meta.env.MODE !== "development") {
+  ReactGA.initialize("G-0MP0S0WEGZ");
+}
 
 function App() {
   return <TicTacToe />;
